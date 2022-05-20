f1bf228441307d9b420b90fefd0572d5f3535a07
Nehal Shyam <75104500+nehalshyam12@users.noreply.github.com>
2022-05-20 08:30:42 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,16 @@
+AFRAME.registerComponent('move-box',{
+    schema:{
+        moveX:{type:'number',default:1},
+
+    },
+
+tick:function(){
+    this.data.moveX = this.data.moveX +0.01;
+
+    var pos = this.el.getAttribute('position');
+
+    pos.X = this.data.moveX;
+
+    this.el.setAttribute('position',{x:pos.X,y:pos.y,z:pos.z})
+}
+})
---
+++ Log.js
@@ -0,0 +1,8 @@
+AFRAME.registerComponent('log',{
+    schema:{
+        message:{type:'string',default:'hello world'}
+    },
+    init:function(){
+        console.log(this.data.message)
+    } 
+})
